9589aa21b23abde975b8a97b62a75c32d1b80679
Paodt <odehpatrick255@gmail.com>
2022-12-04 00:46:04 -0800

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last = number % 10
+        print("{:d}".format(last), end='')
+    if number < 0:
+        last = number % -10
+        last = last * -1
+        return last
