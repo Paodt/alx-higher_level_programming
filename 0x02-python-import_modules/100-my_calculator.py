b334e1f6f798e69559685b1c97e0e846b9479d47
Paodt <odehpatrick255@gmail.com>
2022-12-05 16:55:52 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,31 +2,22 @@
 
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv)
-    if n != 3:
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-    c = sys.argv[2]
-
-    if c != '+' and c != '-' and c != '*' and c != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    from calculator_1 import add, sub, mul, div
-
+    
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if c == '+':
-        add(a, b)
+    if sys.argv[2] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif c == '-':
-        sub(a, b)
+    elif sys.argv[2] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif c == '*':
-        mul(a, b)
+    elif sys.argv[2] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        div(a, b)
+    elif sys.argv[2] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
