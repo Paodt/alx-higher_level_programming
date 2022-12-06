2775aa6502a28a443092110aca039a90c282dbb8
Paodt <odehpatrick255@gmail.com>
2022-12-06 05:38:43 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     length = len(my_list)
     id = my_list[idx]
     if idx < 0:
-        return None
+        return (None)
     if idx > length - 1:
-        return None
-    return id
+        return (None)
+    return (id)
