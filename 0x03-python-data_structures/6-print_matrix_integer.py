245898bc36d31db9ea8f636bf190d1968e74ec88
Paodt <odehpatrick255@gmail.com>
2022-12-10 02:36:15 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    mtx1 = matrix[0]
+    for i in mtx1:
+        print("{:d}".format(i), end=' ')
+    print()
+    mtx2 = matrix[1]
+    for i in mtx2:
+        print("{:d}".format(i), end=' ')
+    print()
+    mtx3 = matrix[2]
+    for i in mtx3:
+        print("{:d}".format(i), end=' ')
+    print()
