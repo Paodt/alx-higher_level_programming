dc79b3ec49cc479ba01a153a275ac4dc405af156
Paodt <odehpatrick255@gmail.com>
2022-12-28 17:18:28 -0800

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_num = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    if not roman_string or None:
+        return 0
+    else:
+        result = 0
+        for a, b in zip(roman_string, roman_string[1:]):
+            if roman_num[a] < roman_num[b]:
+                result -= roman_num[a]
+            else:
+                result += roman_num[a]
+        return result + roman_num[roman_string[-1]]
